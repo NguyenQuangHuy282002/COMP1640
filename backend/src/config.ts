c465bc701dd1c343f361ab5c7f4e7a3cffc0d356
NguyenQuangHuy282002 <huy282002@gmail.com>
2023-02-11 23:13:52 +0700

    Update backend

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,7 +6,7 @@ export async function ReadConfig() {
   const resolvedir = (dir: any) => (dir ? path.resolve(process.cwd(), dir) : undefined)
   const config = {
     server: {
-      port: process.env.PORT || 8080,
+      port: typeof process.env.PORT === 'number' ? process.env.PORT : 8080,
     },
     database: {
       db_url: process.env.DB_URL,
