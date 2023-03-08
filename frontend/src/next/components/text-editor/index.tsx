cff44e4014bc7d423eb1d56c9b06cfaf8fff9f9a
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2023-03-08 09:13:33 +0700

    Update fe

--- frontend/src/next/components/text-editor/index.tsx
+++ frontend/src/next/components/text-editor/index.tsx
@@ -1,9 +1,64 @@
-import { Editor, EditorState } from 'draft-js'
-import { useState } from 'react'
-import 'draft-js/dist/Draft.css'
+import { Editor } from 'react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
-export default function TextEditor() {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
-
-  return <Editor editorState={editorState} onChange={setEditorState} />
+interface IRichTextEditor {
+  editorState: string
+  setEditorState: (val: string) => void
+  placeholder?: string
 }
+
+function RichTextEditor(props: IRichTextEditor) {
+  const { editorState, setEditorState, placeholder } = props
+
+  const onChange = async (value: any) => {
+    setEditorState(value)
+  }
+
+  const toolBarOptions = {
+    inline: { inDropdown: true },
+    list: { inDropdown: true },
+    textAlign: { inDropdown: true },
+    link: { inDropdown: true },
+    history: { inDropdown: true },
+    image: {
+      // uploadCallback: uploadImageCallBack,
+      alt: { present: true, mandatory: true },
+    },
+  }
+
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={(value: any) => {
+          onChange(value)
+        }}
+        toolbarStyle={{
+          background: '#f7fbfa',
+          border: '0.5px solid #ccc',
+        }}
+        editorStyle={{
+          border: '1px #ccc solid',
+          background: 'white',
+          padding: '0 10px',
+          wordBreak: 'break-word',
+          fontWeight: '400',
+          resize: 'vertical',
+          height: '160px',
+          width: '100%',
+          borderRadius: '5px',
+          overflow: 'auto',
+          boxShadow: 'inset 0px 1px 1px #ccc',
+        }}
+        toolbar={toolBarOptions}
+        stripPastedStyles
+        ariaLabel="draftEditor"
+        placeholder={placeholder || 'Description'}
+      />
+    </div>
+  )
+}
+export default RichTextEditor
